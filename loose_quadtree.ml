9df82182068ad2ca2c8099bc27ae9d57cdf41a18
Stephen Sherratt <stephen@sherra.tt>
2018-10-02 09:21:26 +1000

    More refactor

--- loose_quadtree.ml
+++ loose_quadtree.ml
@@ -1,6 +1,13 @@
 open! Core
 
-module Bounding_rect = struct
+module Bounding_rect : sig
+  type t [@@deriving sexp_of]
+
+  val create : centre:(float * float) -> width:float -> height:float -> t
+  val create_with_nw : x:float -> y:float -> width:float -> height:float -> t
+  val centre : t -> float * float
+  val max_dimension : t -> float
+end = struct
   type t =
     { centre : (float * float)
     ; width : float
@@ -9,7 +16,7 @@ module Bounding_rect = struct
 
   let create = Fields.create
 
-  let create_with_top_left ~x ~y ~width ~height =
+  let create_with_nw ~x ~y ~width ~height =
     let centre_x = x +. width /. 2. in
     let centre_y = y +. height /. 2. in
     { centre = (centre_x, centre_y) ; width ; height }
@@ -17,22 +24,25 @@ module Bounding_rect = struct
   let max_dimension t = Float.max t.width t.height
 end
 
-module Bounding_square = struct
+module Node_square : sig
+  type t
+
+  val create_with_corner_at_origin : size:float -> t
+  val size : t -> float
+  val centre : t -> float * float
+  val nw : t -> t
+  val ne : t -> t
+  val se : t -> t
+  val sw : t -> t
+end = struct
   type t =
     { centre : (float * float)
     ; size : float
     } [@@deriving fields]
 
-  let with_corner_at_origin size =
+  let create_with_corner_at_origin ~size =
     let half_size = size /. 2. in
     { centre = (half_size, half_size) ; size }
-
-  let of_bounding_rect bounding_rect =
-    let centre = Bounding_rect.centre bounding_rect in
-    let size = Bounding_rect.max_dimension bounding_rect in
-    { centre ; size }
-
-  let to_bounding_rect { centre ; size } = Bounding_rect.create ~centre ~width:size ~height:size
 
   let corner { centre = (x, y) ; size } x_sign y_sign =
     let offset = size /. 4. in
@@ -41,13 +51,21 @@ module Bounding_square = struct
     let size = size /. 2. in
     { centre = (x, y) ; size }
 
-  let top_left t = corner t (-1.) (-1.)
-  let top_right t = corner t 1. (-1.)
-  let bottom_left t = corner t (-1.) 1.
-  let bottom_right t = corner t 1. 1.
+  let nw t = corner t (-1.) (-1.)
+  let ne t = corner t 1. (-1.)
+  let sw t = corner t (-1.) 1.
+  let se t = corner t 1. 1.
 end
 
-module To_insert = struct
+module To_insert : sig
+  type 'a t
+
+  val create : bounding_rect:Bounding_rect.t -> value:'a -> 'a t
+  val max_dimension : 'a t -> float
+  val centre : 'a t -> (float * float)
+  val bounding_rect : 'a t -> Bounding_rect.t
+  val value : 'a t -> 'a
+end = struct
   type 'a t =
     { bounding_rect : Bounding_rect.t
     ; value : 'a
@@ -55,37 +73,46 @@ module To_insert = struct
 
   let create = Fields.create
 
-  let bounding_square t = Bounding_square.of_bounding_rect (t.bounding_rect)
-
   let max_dimension t = Bounding_rect.max_dimension t.bounding_rect
 
   let centre t = Bounding_rect.centre t.bounding_rect
 end
 
-module Node = struct
+module Node : sig
+  type 'a t [@@deriving sexp_of]
+
+  val empty : 'a t
+  val empty_with_nw : 'a t -> 'a t
+  val insert : 'a t -> node_square:Node_square.t -> 'a To_insert.t -> 'a t
+end = struct
   type 'a t =
     | Leaf
     | Tree of 'a tree
     [@@deriving sexp_of]
   and 'a tree =
-    { top_left : 'a t
-    ; top_right : 'a t
-    ; bottom_left : 'a t
-    ; bottom_right : 'a t
+    { nw : 'a t
+    ; ne : 'a t
+    ; sw : 'a t
+    ; se : 'a t
     ; data : (Bounding_rect.t * 'a) list
     } [@@deriving sexp_of]
 
   let empty_tree =
-    { top_left = Leaf
-    ; top_right = Leaf
-    ; bottom_left = Leaf
-    ; bottom_right = Leaf
+    { nw = Leaf
+    ; ne = Leaf
+    ; sw = Leaf
+    ; se = Leaf
     ; data = []
     }
 
-  let rec insert t ~node_square ({ To_insert.bounding_rect; value } as to_insert)  =
-    if Float.(Bounding_rect.max_dimension bounding_rect < Bounding_square.size node_square)
+  let empty = Leaf
+  let empty_with_nw nw = Tree { empty_tree with nw }
+
+  let rec insert t ~node_square to_insert  =
+    let bounding_rect = To_insert.bounding_rect to_insert in
+    if Float.(Bounding_rect.max_dimension bounding_rect < Node_square.size node_square)
     then
+      let value = To_insert.value to_insert in
       match t with
       | Leaf -> Tree { empty_tree with data = [ bounding_rect, value ] }
       | Tree tree -> Tree { tree with data = (bounding_rect, value) :: tree.data }
@@ -95,24 +122,24 @@ module Node = struct
         | Tree tree -> tree
       in
       let bounding_rect_x, bounding_rect_y = Bounding_rect.centre bounding_rect in
-      let node_x, node_y = Bounding_square.centre node_square in
+      let node_x, node_y = Node_square.centre node_square in
       if Float.(bounding_rect_x < node_x)
       then
         if Float.(bounding_rect_y < node_y)
         then
-          let node_square = Bounding_square.top_left node_square in
-          Tree { tree with top_left = insert tree.top_left ~node_square to_insert }
+          let node_square = Node_square.nw node_square in
+          Tree { tree with nw = insert tree.nw ~node_square to_insert }
         else
-          let node_square = Bounding_square.bottom_left node_square in
-          Tree { tree with bottom_left = insert tree.bottom_left ~node_square to_insert }
+          let node_square = Node_square.sw node_square in
+          Tree { tree with sw = insert tree.sw ~node_square to_insert }
       else
         if Float.(bounding_rect_y < node_y)
         then
-          let node_square = Bounding_square.top_right node_square in
-          Tree { tree with top_right = insert tree.top_right ~node_square to_insert }
+          let node_square = Node_square.ne node_square in
+          Tree { tree with ne = insert tree.ne ~node_square to_insert }
         else
-          let node_square = Bounding_square.bottom_right node_square in
-          Tree { tree with bottom_right = insert tree.bottom_right ~node_square to_insert }
+          let node_square = Node_square.se node_square in
+          Tree { tree with se = insert tree.se ~node_square to_insert }
 end
 
 type 'a t =
@@ -120,45 +147,42 @@ type 'a t =
   ; size : float
   } [@@deriving sexp_of]
 
-let empty_with_initial_size size = { root = Node.Leaf ; size }
+let empty_with_initial_size size = { root = Node.empty ; size }
 
 let rec grow_to_size { root ; size } target_size =
   if target_size <= size
   then { root ; size }
   else
-    let root_tree = { Node.empty_tree with top_left = root } in
-    let next = { root = Node.Tree root_tree ; size = size *. 2. } in
+    let root = Node.empty_with_nw root in
+    let next = { root ; size = size *. 2. } in
     grow_to_size next target_size
 
-let grow_to_fit t bounding_square =
+let grow_to_fit t bounding_rect =
   let centre_max_dimension =
-    let x, y = Bounding_square.centre bounding_square in
+    let x, y = Bounding_rect.centre bounding_rect in
     Float.max x y
   in
   let target_size =
-    Float.max centre_max_dimension (Bounding_square.size bounding_square)
+    Float.max centre_max_dimension (Bounding_rect.max_dimension bounding_rect)
   in
   grow_to_size t target_size
 
 let insert t bounding_rect value =
   let to_insert = To_insert.create ~bounding_rect ~value in
-  let t = grow_to_fit t (To_insert.bounding_square to_insert) in
-  let node_square = Bounding_square.with_corner_at_origin t.size in
+  let t = grow_to_fit t bounding_rect in
+  let node_square = Node_square.create_with_corner_at_origin ~size:t.size in
   let root = Node.insert t.root ~node_square to_insert in
   { t with root }
 
 let%expect_test "insert" =
-  let a = Bounding_rect.create_with_top_left ~x:42. ~y:27. ~width:51. ~height:9. in
+  let a = Bounding_rect.create_with_nw ~x:42. ~y:27. ~width:51. ~height:9. in
   let t = empty_with_initial_size 32. in
   let t = insert t a "a" in
   printf !"%{sexp:string t}\n" t;
   [%expect {|
     ((root
       (Tree
-       ((top_left
-         (Tree
-          ((top_left Leaf) (top_right Leaf) (bottom_left Leaf)
-           (bottom_right Leaf) (data ()))))
-        (top_right Leaf) (bottom_left Leaf) (bottom_right Leaf)
+       ((nw (Tree ((nw Leaf) (ne Leaf) (sw Leaf) (se Leaf) (data ())))) (ne Leaf)
+        (sw Leaf) (se Leaf)
         (data ((((centre (67.5 31.5)) (width 51) (height 9)) a))))))
      (size 128)) |}]
